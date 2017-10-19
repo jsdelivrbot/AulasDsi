eb027b45a41455e13ac4b7fe3b4f43a36aec475b
Fernando D'Agostini <fdagostini@gmail.com>
2017-10-19 18:08:06 -0200

    Exemplo básico.

--- aula14/frontend/src/app/app.module.ts
+++ aula14/frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AlunoComponent } from './aluno/aluno.component';
+import { AlunoService } from './aluno/aluno.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AlunoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
