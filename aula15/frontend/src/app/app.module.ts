bad1de92390b962a9a512a8a1896bffe16c94a1a
Fernando D'Agostini <fdagostini@gmail.com>
2017-10-24 16:37:47 -0200

    Roteamento básico.

--- aula15/frontend/src/app/app.module.ts
+++ aula15/frontend/src/app/app.module.ts
@@ -2,20 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AlunoComponent } from './aluno/aluno.component';
 import { AlunoService } from './aluno/aluno.service';
 
+import { MenuComponent } from './menu/menu.component';
+import { ErroComponent } from './erro/erro.component';
+import { OpcaoAComponent } from './opcao-a/opcao-a.component';
+import { OpcaoBComponent } from './opcao-b/opcao-b.component';
+
+import { appRoutes } from './app.routes';
+
 @NgModule({
   declarations: [
     AppComponent,
-    AlunoComponent
+    AlunoComponent,
+    MenuComponent,
+    ErroComponent,
+    OpcaoAComponent,
+    OpcaoBComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    )
   ],
   providers: [AlunoService],
   bootstrap: [AppComponent]
---
+++ aula15/frontend/src/app/opcao-a/opcao-a.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opcao-a',
+  templateUrl: 'opcao-a.component.html',
+})
+export class OpcaoAComponent implements OnInit {
+  constructor() {  }
+
+  ngOnInit() {}
+}
