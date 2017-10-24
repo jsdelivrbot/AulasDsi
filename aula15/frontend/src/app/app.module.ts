1f8e61c30ec11fcc09e2c745686b93fb2165592b
Fernando D'Agostini <fdagostini@gmail.com>
2017-10-24 18:12:35 -0200

    bootstrap.

--- aula15/frontend/src/app/app.module.ts
+++ aula15/frontend/src/app/app.module.ts
@@ -13,6 +13,10 @@ import { ErroComponent } from './erro/erro.component';
 import { OpcaoAComponent } from './opcao-a/opcao-a.component';
 import { OpcaoBComponent } from './opcao-b/opcao-b.component';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { appRoutes } from './app.routes';
 
 @NgModule({
@@ -31,7 +35,9 @@ import { appRoutes } from './app.routes';
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: false } // <-- debugging purposes only
-    )
+    ),
+    BsDropdownModule.forRoot(),
+    CollapseModule.forRoot()
   ],
   providers: [AlunoService],
   bootstrap: [AppComponent]
--- aula15/frontend/src/app/menu/menu.component.ts
+++ aula15/frontend/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() {  }
+  public isCollapsed = false;
+
+  constructor(
+    private router: Router
+  ) {  }
 
   ngOnInit() {}
+
+  public activeRoute(routename: string): boolean {
+    console.log(this.router.url);
+    return this.router.url.indexOf(routename) > -1;
+  }
 }
